2312e63558b318a9434cbef502a0a9d6e8e1d163
Necati Efe Aslan <necatiefeaslan5@gmail.com>
2025-05-11 16:35:37 +0300

    Yeni

--- obj/Debug/net9.0/Kutuphane.AssemblyInfo.cs
+++ obj/Debug/net9.0/Kutuphane.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370e17727b54701e754904e8d8a36ffeb83a42fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30ae863e8990f9cf23aef80c081eedf88d69075")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
